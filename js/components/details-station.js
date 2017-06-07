69b18b49fe3da020c69021be8d69e37c42c602ca
Fiorellacr24 <fiorella.0624@gmail.com>
2017-06-07 13:37:42 -0500

    modificando filter

--- js/components/details-station.js
+++ js/components/details-station.js
@@ -3,6 +3,7 @@
 const detailStation = (data, update) => {
     const container = $("<section class='container-map'></section>")
     const divmap = $("<div id='map'></div>");
+    const detail = $("<div class='container-detail'></div>");
     const nombre = $("<h4>"+ data.name +"</h4>");
     const btn86 = $("<button>86</button>");
     const btn90 = $("<button>90</button>");
--- js/components/search.js
+++ js/components/search.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const stationItem = (data, update) => {
+    const parent = $("<div class='station'></div>");
+    const row = $("<div class='row'>")
+    const col = $("<div class='col s12'>")
+    const nombre = $("<h5>"+ data.name +"</h5>");
+    const direccion = $("<p>"+ data.address +"</p>");
+    const distrito = $("<p>"+ data.district +"</p>");
+    const iconmap = $("<span><i class='fa fa-map'></i></span>");
+    //const stationfilter = $('<div class="filter"></div>');
+
+    row.append(col);
+    col.append(nombre);
+    col.append(direccion);
+    col.append(distrito);
+    col.append(iconmap);
+    parent.append(row);
+
+    return parent;
+}
+
+const Station = () => {
+    const stationContainer = $('<div class="station-container"></div>');
+    state.stations.forEach((data)=>{
+        stationContainer.append(stationItem(data));
+    });
+
+    return stationContainer;
+}
+
 const Search = (update) => {
     const parent = $("<div class='search'></div>");
     const row = $("<div class='row'></div>");
